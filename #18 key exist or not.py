b4c8fc15d369bdf906209133d7c090ee262ff286
Muhammed Hashir <muhammedhashir2727@gmail.com>
2022-03-04 22:30:22 +0530

    programs 18-37

---
+++ #18 key exist or not.py
@@ -0,0 +1,6 @@
+d={'name':'vinu','course':'MCA','sem':4}
+k=input("Enter key to check : ")
+if(k in d.keys()):
+    print("Key already exists")
+else:
+    print("Key doesn't exists")
---
+++ #25 b available or not.py
@@ -0,0 +1,3 @@
+l=[100,2,68,73,57,1225]
+c=int(input("Enter the number to check : "))
+print("Available") if c in l else print("Not available")
